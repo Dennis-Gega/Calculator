58011c51f0a891b1e2246ad018e3164f434e656c
Dennis Gega <dennis.gega@uconn.edu>
2024-04-14 23:40:46 -0400

    Fixed bug where entering a number after getting a result inserted the number after the result

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ let currentTotal = '';
 function displayCalculation() {
     document.querySelector('.calculation').innerText = currentTotal;
 }
-    
+
 function calculateResult() {
     document.querySelector('.calculation').innerText = eval(currentTotal);
     currentTotal = eval(currentTotal);
-} 
+}
+
+function appendToTotal(value) {
+    // If currentTotal contains a result, clear it before appending the new value
+    if (typeof currentTotal === 'number') {
+        currentTotal = '';
+    }
+    currentTotal += value;
+    displayCalculation();
+}
+
+function clearTotal() {
+    currentTotal = '';
+    displayCalculation();
+}
